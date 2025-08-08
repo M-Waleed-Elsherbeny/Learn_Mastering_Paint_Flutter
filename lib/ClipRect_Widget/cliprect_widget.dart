a7b130a121a4aef380c46c1c861eb6e67ab4465c
Mohammed Waleed <m.elsherbeny100@gmail.com>
2025-08-08 17:09:00 +0300

    Initial Commit

---
+++ lib/ClipRect_Widget/cliprect_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClipRectWidget extends StatelessWidget {
+  const ClipRectWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: FlutterLogo(size: 300,));
+  }
+}
